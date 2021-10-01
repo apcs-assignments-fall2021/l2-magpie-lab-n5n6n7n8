a664b6dceb8c32bc0816a00899e4d05166d6f397
natewong <nwwong17@gmail.com>
2021-09-30 21:44:11 -0700

    Made progress on a chatbot for APCS lab

--- src/main/java/Magpie.java
+++ src/main/java/Magpie.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 /**
  * A program to carry on conversations with a human user.
  * This is the initial version that:  
@@ -30,20 +32,39 @@ public class Magpie
      */
     public String getResponse(String statement)
     {
-        String response = "";
-        if (statement.indexOf("no") >= 0)
+        String response;
+        if (findWord(statement,"no")>= 0)
         {
-            response = "Why so negative?";
+            response = "Why not?";
         }
-        else if (statement.indexOf("mother") >= 0
-                || statement.indexOf("father") >= 0
-                || statement.indexOf("sister") >= 0
-                || statement.indexOf("brother") >= 0)
-        {
-            response = "Tell me more about your family.";
+        else if (findWord(statement, "mother") >= 0
+                || findWord(statement, "father") >= 0
+                || findWord(statement, "sister") >= 0
+                || findWord(statement, "brother") >= 0) {
+            response = "How is your family doing?";
         }
-        else
-        {
+        else if(findWord(statement, "I want")>=0) {
+            response = transformIWantStatement(statement).trim();
+        }
+        else if(findWord(statement, "I ")<findWord(statement, "you")) {
+            response = transformIYouStatement(statement).trim();
+        }
+        else if(findWord(statement, "cat")>=0||findWord(statement, "dog")>=0) {
+            response = "Tell me more about your pets";
+        }
+        else if(findWord(statement, "Mr.")>=0||findWord(statement, "Mrs.")>=0||findWord(statement, "Ms.")>=0) {
+            response = "Your teacher sounds interesting, tell me more.";
+        }
+        else if(findWord(statement, "ice cream")>=0) {
+            response = "My favorite ice cream flavor is vanilla.";
+        }
+        else if(findWord(statement, "video game")>=0) {
+            response = "Cool, I like to play Tetris but only on weekends.";
+        }
+        else if(statement.trim().equals("")) {
+            response = "Hello? Are you there?";
+        }
+        else {
             response = getRandomResponse();
         }
         return response;
@@ -76,24 +97,41 @@ public class Magpie
         {
             response = "You don't say.";
         }
-    
         return response;
     }
-
-    // Checks to see if the String word appears as a whole word
-    // in the String str (in this case, a "whole word" means that 
-    // word is not just a substring of some larger word in str)
-
-    // This method should work regardless of the capitalization 
-    // of str or word
-
-    // The method returns the index of the first character in word
-    // if it is found, and returns -1 otherwise. 
-    public int findWord(String str, String word) {
+    public int findWord(String str, String word) { //for this to work, we need to make sure the word is not surrounded by letters
+        str = str.toUpperCase();
+        word = word.toUpperCase();
+        word = word.trim();
+        int index = str.indexOf(word);
+        if(index==-1)
+        {
+            return -1;
+        }
+        Boolean rclear=false; //checks right side of str for letters
+        Boolean lclear=false; //checks left side of str for letters
+        if(index==0) //is word at the very beginning? or is the character in front of word not a letter?
+        {
+            lclear=true;
+        }
+        else if(!Character.isLetter(str.charAt(index-1)))
+        {
+            lclear=true;
+        }
+        if(index==str.length()-word.length()) //is word at very end? or is the character to the right of the str not a lettter
+        {
+            rclear=true;
+        }
+        else if(!Character.isLetter(str.charAt(index+word.length())))
+        {
+            rclear=true;
+        }
+        if(rclear&&lclear) //left side AND right side must both be clear of letters to return index
+        {
+            return index;
+        }
         return -1;
     }
-
-    
     // We will work on the following methods later!
 
     /**
@@ -104,10 +142,9 @@ public class Magpie
      */
     public String transformIWantStatement(String statement)
     {
-        //your code here
-        return "";
+        int index = findWord(statement, "I want ");
+        return "Would you really be happy if you had " + statement.substring(index+7) + "?";
     }
-
     /**
      * Take a statement with "I <something> you" and transform it into 
      * "Why do you <something> me?"
@@ -116,8 +153,9 @@ public class Magpie
      */
     public String transformIYouStatement(String statement)
     {
-        //your code here
-        return "";
+        int index = findWord(statement, "I");
+        int index2 = statement.indexOf("you", index);
+        return "Why do you " + statement.substring(index+2, index2) + "me?";
     }
 
     /**
--- src/main/java/MagpieRunner.java
+++ src/main/java/MagpieRunner.java
@@ -17,13 +17,13 @@ public class MagpieRunner
     	System.out.println(maggie.getGreeting());
     	Scanner in = new Scanner (System.in);
     	String statement = in.nextLine();
-    	
+
     	while (!statement.equals("Bye"))
     	{
     		System.out.println(maggie.getResponse(statement));
     		statement = in.nextLine();
         }
-        
+
         in.close();
     }
 }
